057e2f6a61bb525b4c3df1a0d582995647a8843c
inaies <inaie.moscal@gmail.com>
2023-02-01 17:28:50 -0300

    testes

--- mouse.c
+++ mouse.c
@@ -1,16 +1,43 @@
 #include "mouse.h"
 #define tam 10
 
-int verifica_joia_selecionada(MATRIX_t **m, int x_joia, int y_joia)
+void verifica_joia_selecionada(MATRIX_t **m, int joia_x, int joia_y)
 {
     for (int i = 0; i < tam; i++)
     {
         for (int j = 0; j < tam; j++)
         {
-            if(m[i][j].sel == 2)
-                return 1;
+            if((m[i][j].sel == 2)&&((i == (joia_x-1))&& (j == joia_y)))
+            {   
+                if(eh_joia_vizinha(joia_x, joia_y, i, j))
+                    m[i][j].sel = 3;
+                else 
+                    m[i][j].sel = 0;
+                // return 1;
+            }
         }
     }
+    // return 0;
+}
+
+int eh_joia_vizinha(int x_joia, int y_joia, int i, int j)
+{
+    if((j < 10)&&(i < 10))
+    {
+        if((j == y_joia) && ((i == (x_joia - 1))||(i == (x_joia + 1))))
+            return 1;
+        if((i == x_joia) && ((j == (y_joia - 1))||(j == (y_joia + 1))))
+            return 1;
+    }
+    // else if(j == 10)
+    // {
+    //     if((j == y_joia)&&((i == (x_joia - 1))||(i == (x_joia + 1))))
+    //         return 1;
+    // }
+    // else if(i == 10)
+    // {
+    //     if()
+    // }
     return 0;
 }
 
@@ -26,8 +53,11 @@ int mouse_joia(MATRIX_t **m, int mouse_x, int mouse_y, int click)
                 {
                     if(m[i][j].sel == 2)
                         m[i][j].sel = 0;
-                    else if(!(verifica_joia_selecionada(m, i, j)))
+                    else
+                    {
+                        verifica_joia_selecionada(m, i, j);
                         m[i][j].sel = 2;
+                    }
                 }
                 else if(m[i][j].sel != 2)
                     m[i][j].sel = 1;
--- mouse.h
+++ mouse.h
@@ -3,7 +3,9 @@
 
 #include "matrix.h"
 
-int verifica_joia_selecionada(MATRIX_t **m, int x_joia, int y_joia);
+void verifica_joia_selecionada(MATRIX_t **m, int joia_x, int joia_y);
+
+int eh_joia_vizinha(int x_joia, int y_joia, int i, int j);
 
 int mouse_joia(MATRIX_t **m, int mouse_x, int mouse_y, int click);
 
